8115823691d76f8af5c3bad665da5357b2e805e4
Mattias Andrée <maandree@kth.se>
2022-02-13 00:32:56 +0100

    Fix typo
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- test.c
+++ test.c
@@ -38,7 +38,7 @@ assert_streq_(const char *result, const char *expect, const char *code, int line
 {
 	if (nulstrcmp(result, expect)) {
 		if (from_lineno)
-			fprintf(stderr, "Assertion at line %i, form line %i failed:\n", lineno, from_lineno);
+			fprintf(stderr, "Assertion at line %i, from line %i failed:\n", lineno, from_lineno);
 		else
 			fprintf(stderr, "Assertion at line %i failed:\n", lineno);
 		fprintf(stderr, "\tcode:     %s\n", code);
@@ -55,7 +55,7 @@ assert_zueq_(size_t result, size_t expect, const char *code, int lineno)
 {
 	if (result != expect) {
 		if (from_lineno)
-			fprintf(stderr, "Assertion at line %i, form line %i failed:\n", lineno, from_lineno);
+			fprintf(stderr, "Assertion at line %i, from line %i failed:\n", lineno, from_lineno);
 		else
 			fprintf(stderr, "Assertion at line %i failed:\n", lineno);
 		fprintf(stderr, "\tcode:     %s\n", code);
